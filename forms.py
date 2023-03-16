5eeabc4e51bdf94e080ee6a2233f82156d92993b
Sergey_Ekshevits <sekshevits@gmail.com>
2023-03-16 18:31:16 +0400

    multiple checkbox on category selection

--- forms.py
+++ forms.py
@@ -7,6 +7,9 @@ from flask_ckeditor import CKEditorField
 # from wtforms.fields.html5 import EmailField
 # from flask_uploads import UploadSet, IMAGES
 # images = UploadSet('images', IMAGES)
+class SelectFieldMultiCheckbox(SelectMultipleField):
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
 class LoginForm(FlaskForm):
     email=EmailField("E-mail", validators=[DataRequired(), Email(message='Only e-mail is allowed, e.g test@test.py')])
     password = PasswordField("Password", validators=[DataRequired(), Length(min=4,max=99)])
@@ -29,7 +32,7 @@ class PostField(FlaskForm):
     title = StringField("Post title", validators=[DataRequired(),Length(min=4,max=99)])
     body = CKEditorField("Body", validators=[DataRequired(), Length(min=20,max=1199)])
     post_pic = FileField('Post picture', default=None, validators=[FileAllowed(['jpg','jpeg','png'],"Only images!")])
-    category = SelectMultipleField ('Категории', default=None, coerce=int)
+    category = SelectFieldMultiCheckbox ('Категории', default=None, coerce=int)
     submit = SubmitField("Отправить")
 
 class CommentField(FlaskForm):
@@ -39,5 +42,6 @@ class SelectPostsFilter(FlaskForm):
     my_post = BooleanField('My Posts', default="checked")
     selection = SelectField('Select posts', choices=[('all','All posts'),('last_day', 'Last day'),('last_week','Last week'),('last_month', 'Last month')])
     submit = SubmitField("Искать")
+
 class CategoryFilter(FlaskForm):
     category = BooleanField('Categories', default="checked")
